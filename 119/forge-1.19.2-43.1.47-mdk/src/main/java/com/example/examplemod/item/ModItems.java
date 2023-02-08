9acbe6e4fd590fef109291ea9bd1d9f9b910060d
GJQCSJ <sgrlian3@liverpool.ac.uk>
2023-02-08 21:51:43 +0000

    A

--- 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/item/ModItems.java
+++ 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/item/ModItems.java
@@ -1,14 +1,20 @@
 package com.example.examplemod.item;
 
 import com.example.examplemod.comp208mod;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, comp208mod.MOD_ID);
+
+    public static final RegistryObject<Item> TEST = ITEMS.register("test_item",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
