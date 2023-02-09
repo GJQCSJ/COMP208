e730c913048c30fa3307712081dd8fcdefb7f3bd
GJQCSJ <sgrlian3@liverpool.ac.uk>
2023-02-09 18:13:27 +0000

    A

--- 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/block/ModBlocks.java
+++ 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/block/ModBlocks.java
@@ -3,10 +3,12 @@ package com.example.examplemod.block;
 import com.example.examplemod.comp208mod;
 import com.example.examplemod.item.ModCreativeModeTab;
 import com.example.examplemod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,16 +22,31 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS1 =
             DeferredRegister.create(ForgeRegistries.BLOCKS, comp208mod.MOD_ID);
 
-    public static final RegistryObject<Block> TEST_BLOCK1 = registerBlock("test_block1",
-            () -> new Block(BlockBehaviour.Properties.of(Material.SAND).
-                    jumpFactor(10).strength(1f, 9)
-                    .requiresCorrectToolForDrops()),
+    public static final RegistryObject<Block> TEMPLATE_BLOCK =registerBlock("test_block_template",
+            () -> new Block(BlockBehaviour.Properties.of(Material.DIRT).strength(1f)),
             ModCreativeModeTab.Test_Demo);
-            /*Create and register a new block, properties inside brackets:
+                /*Create and register a new block, properties inside brackets:
             the name of it,
              supplier and type or
               in game properties (may in hexadecimal)
               of block*/
+
+    public static final RegistryObject<Block> TEST_BLOCK1 = registerBlock("test_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.SAND).
+                    jumpFactor(10).strength(6f, 9)
+                    .requiresCorrectToolForDrops()),
+            ModCreativeModeTab.Test_Demo);
+
+
+            public static final RegistryObject<Block> TEST_BLOCK2 =registerBlock("test_block_ore",
+                    () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                            .strength(6f).requiresCorrectToolForDrops(),
+                            UniformInt.of(5, 9)
+                            /*UniformInt.of will let block drop experience when being excavated,
+                            ,two values means the range of experience it would drop*/
+                    ),
+                    ModCreativeModeTab.Test_Demo);
+
 
     private static <T extends Block>RegistryObject<T> registerBlock(
             String name, Supplier<T> block, CreativeModeTab tab){
