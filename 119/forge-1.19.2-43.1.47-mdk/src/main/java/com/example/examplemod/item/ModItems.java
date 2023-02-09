f2a4a8f46cf8e19acf34f03bd83708f37cc32258
GJQCSJ <sgrlian3@liverpool.ac.uk>
2023-02-09 15:57:49 +0000

    A

--- 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/item/ModItems.java
+++ 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/item/ModItems.java
@@ -13,9 +13,9 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, comp208mod.MOD_ID);
 
     public static final RegistryObject<Item> TEST = ITEMS.register("test_item",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.Test_Demo)));
     public static final RegistryObject<Item> TEST2 = ITEMS.register("test_item2",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.Test_Demo)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
