ce0c27ed943c679550bde5ef05a38e1e93d3cda8
GJQCSJ <sgrlian3@liverpool.ac.uk>
2023-02-09 17:29:45 +0000

    A

--- 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/block/ModBlocks.java
+++ 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/block/ModBlocks.java
@@ -1,14 +1,49 @@
 package com.example.examplemod.block;
 
 import com.example.examplemod.comp208mod;
+import com.example.examplemod.item.ModCreativeModeTab;
+import com.example.examplemod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS1 =
             DeferredRegister.create(ForgeRegistries.BLOCKS, comp208mod.MOD_ID);
+
+    public static final RegistryObject<Block> TEST_BLOCK1 = registerBlock("test_block1",
+            () -> new Block(BlockBehaviour.Properties.of(Material.SAND).
+                    jumpFactor(10).strength(1f, 9)
+                    .requiresCorrectToolForDrops()),
+            ModCreativeModeTab.Test_Demo);
+            /*Create and register a new block, properties inside brackets:
+            the name of it,
+             supplier and type or
+              in game properties (may in hexadecimal)
+              of block*/
+
+    private static <T extends Block>RegistryObject<T> registerBlock(
+            String name, Supplier<T> block, CreativeModeTab tab){
+        RegistryObject<T> toReturn = BLOCKS1.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+        /*register blocks*/
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(
+            String name, RegistryObject<T> block, CreativeModeTab tab){
+        return ModItems.ITEMS.register(
+                name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
 
 
     public static void register(IEventBus eventBus){
