bc599f9736d12724ccaa71b258a6da149b1f3b9d
GJQCSJ <sgrlian3@liverpool.ac.uk>
2023-02-15 01:23:54 +0000

    A

--- 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/item/ModCreativeModeTab.java
+++ 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/item/ModCreativeModeTab.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTab {
-    public static final CreativeModeTab Test_Demo = new CreativeModeTab("Comp208mod") {
+    public static final CreativeModeTab Test_Demo = new CreativeModeTab("comp208mod") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.TEST.get());
