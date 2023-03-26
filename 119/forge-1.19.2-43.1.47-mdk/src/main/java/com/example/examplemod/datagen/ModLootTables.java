7ceaeb3ef017bdc768c91c1acce9aa430bcaef7d
GJQCSJ <sgrlian3@liverpool.ac.uk>
2023-03-26 20:56:13 +0100

    Updated Trees&Jasmine

--- 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/datagen/ModLootTables.java
+++ 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/datagen/ModLootTables.java
@@ -3,15 +3,15 @@ package com.example.examplemod.datagen;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.loot.LootTableProvider;
 
-public class ModLootTables extends LootTableProvider {
+public class ModLootTables extends BasicLootTableProvider {
     public ModLootTables(DataGenerator dataGeneratorIn){
         super(dataGeneratorIn);
     }
 
-//    @Override
-//    protected void addTables(){
-//
-//    }
+    @Override
+    protected void addTables(){
+
+    }
 }
 
 
