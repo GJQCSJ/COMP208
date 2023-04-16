b94be9f07b4bf3d0a7046a839d30dfedc51421c6
GJQCSJ <sgrlian3@liverpool.ac.uk>
2023-04-16 19:50:50 +0100

    Updated ModelLoader

--- 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/util/Tools.java
+++ 119/forge-1.19.2-43.1.47-mdk/src/main/java/com/example/examplemod/util/Tools.java
@@ -14,28 +14,8 @@ import java.util.function.Function;
 
 public class Tools {
 
-    public static void spawnInWorld(Level level, BlockPos pos, ItemStack remaining) {
-        if (!remaining.isEmpty()) {
-            ItemEntity entityitem = new ItemEntity(level, pos.getX() + .5, pos.getY() + .5, pos.getZ() + .5, remaining);
-            entityitem.setPickUpDelay(40);
-            entityitem.setDeltaMovement(entityitem.getDeltaMovement().multiply(0, 1, 0));
-            level.addFreshEntity(entityitem);
-        }
-    }
 
-    public static void teleport(ServerPlayer entity, ServerLevel destination, BlockPos pos, boolean findTop) {
-        entity.changeDimension(destination, new ITeleporter() {
-            @Override
-            public Entity placeEntity(Entity entity, ServerLevel currentWorld, ServerLevel destWorld, float yaw, Function<Boolean, Entity> repositionEntity) {
-                entity = repositionEntity.apply(false);
-                int y = pos.getY();
-                if (findTop) {
-                    y = destination.getHeight(Heightmap.Types.WORLD_SURFACE_WG, pos.getX(), pos.getZ());
-                }
-                entity.teleportTo(pos.getX(), y, pos.getZ());
-                return entity;
-            }
-        });
-    }
+
+
 
 }
